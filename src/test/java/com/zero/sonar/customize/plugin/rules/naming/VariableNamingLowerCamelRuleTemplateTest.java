8d4ca02a4c05a154a780e5202ef6577e07fa1910
ylxb23 <ylxb23@163.com>
2024-05-08 11:35:47 +0800

    遗漏提交

--- src/test/java/com/zero/sonar/customize/plugin/rules/naming/VariableNamingLowerCamelRuleTemplateTest.java
+++ src/test/java/com/zero/sonar/customize/plugin/rules/naming/VariableNamingLowerCamelRuleTemplateTest.java
@@ -8,14 +8,16 @@ import org.sonar.java.checks.verifier.FilesUtils;
  * @author zero
  * @since 2024/5/6 15:58
  */
-public class VariableNamingLowerCamelRuleTest {
+public class VariableNamingLowerCamelRuleTemplateTest {
 
 
     @Test
     public void verify() {
+        VariableNamingLowerCamelRuleTemplate templateInstance = new VariableNamingLowerCamelRuleTemplate();
+        templateInstance.setWhiteKeys("(AO|BO|DAO|DO|DTO|PO|VO|ID|PK)");
         CheckVerifier.newVerifier()
                 .onFile("target/test-classes/java/HumanDO.java")
-                .withCheck(new VariableNamingLowerCamelRule())
+                .withCheck(templateInstance)
                 .withClassPath(FilesUtils.getClassPath("target/test-classes"))
                 .verifyIssues();
     }
--- src/test/resources/java/HumanDO.java
+++ src/test/resources/java/HumanDO.java
@@ -2,11 +2,12 @@ package java;
 
 public class HumanDO {
     private String validName;
+    // 非法的命名
     private String invalidNameKAKA;
 
     public WAKAKAJob transfer(WAKAKANoJob noJob) {
         String name = "No Job";
-        // 非法的命名方式
+        // 非法的命名
         WAKAKAJob jobKKK = new WAKAKAJob();
         jobKKK.setName(name);
         return jobKKK;
